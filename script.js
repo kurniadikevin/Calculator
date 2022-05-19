ee958d01e87e6ac9e8664243f58693b9f54f1b50
kurniadikevin <kurniadikevin@gmail.com>
2022-05-19 14:08:43 +0700

    work but clear btn not perfect after few click and clear btn undone

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ let secondNumber;
 let operatorValue;
 
 let result = document.querySelector('.display');
-let input = document.querySelector('.input');
+let input = document.querySelector('.input');// number for display 
 let allButtons = document.querySelectorAll('.button');
 let eqButton = document.querySelector('.buttonEqual');
 let allOperator = document.querySelectorAll('.operator');
-let inputData = '';
+let inputData = '';// number for data
+let clearBtn = document.querySelector('#clear');
+let deleteBtn = document.querySelector('#delete');
 
 
 
@@ -33,45 +35,89 @@ function clickNumber(){
      allButtons[i].addEventListener( 'click', function(){
        let inputClick = this.textContent;
        inputData += inputClick;
-       input.textContent += inputClick;
+       input.textContent += inputClick;// for display
        console.log(inputClick);// test console log
        })
     }
 };
 
 
-
-// operator on click
-function clickOperator(){
+// operator on click (BACK UP )
+/*
+ function clickOperator(){
     for( let i = 0; i< allOperator.length; i++){
       allOperator[i].addEventListener( 'click', function(){
-      firstNumber = inputData;
+      firstNumber = inputData;// 
       operatorValue = this.id;
       input.textContent += this.textContent;
       inputData = '';
-    
-      
+   
       console.log('firstNum: '+firstNumber);//
       console.log('OPvalue: ' + operatorValue);//
-      
-    })
-    }};
+      }
+      )}
+    };
+    */
+
+    // operator on click
+    function clickOperator(){
+        for( let i = 0; i< allOperator.length; i++){
+          allOperator[i].addEventListener( 'click', function(){
+          if( operatorValue != null){
+            secondNumber = inputData;
+            firstNumber = arithmetic(firstNumber,secondNumber,operatorValue);
+            operatorValue = this.id;
+            input.textContent += this.textContent;// for display
+            inputData = '';
+          } else{
+          firstNumber = inputData;
+          operatorValue = this.id;
+          input.textContent += this.textContent;// for display
+          inputData = '';
+       
+          console.log('firstNum: '+firstNumber);//
+          console.log('OPvalue: ' + operatorValue);//
+          }
+          }
+          )}
+        };
 
 function equal(){
   eqButton.addEventListener('click',function(){
     secondNumber = inputData;
     result.textContent = arithmetic(firstNumber,secondNumber,operatorValue);
     console.log('secondNumber ' +secondNumber);
-
-  })
-
+    })
 };
+
+function clearData(){
+  clearBtn.addEventListener('click',function(){
+    result.textContent= '0';
+    input.textContent='';
+    firstNumber -= firstNumber;
+    secondNumber -= secondNumber;
+    inputData='';  
+  })
+}
+
+// delete last input
+/*
+function deleteNumber(){
+  deleteBtn.addEventListener('click',function(){
+    inputData -= inputClick;
+    input.textContent -= inputClick;
+  })
+}
+*/
 
 
 
 clickNumber();
 clickOperator();
 equal();
+clearData();
+//deleteNumber();
+console.log('firstNum '+firstNumber);
 
-
-// display good but variable for variable secondNumber still fals
+// add function for clear and delete
+// if first number not null firstNumber is result of last result
